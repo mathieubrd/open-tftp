ccde0f190aea45cf7c252a0b8f617ad7afa5dcc6
Mathieu Brochard <mathieu.brochard76@free.fr>
2016-04-28 19:44:49 +0200

    Ajout prototypes routines

--- include/tftp.h
+++ include/tftp.h
@@ -9,20 +9,20 @@
 #include <AdresseInternet.h>
 #include <errno.h>
 
-// OPCode
+// Identifiants opcode
 #define RRQ 1
 #define DATA 3
 #define ACK 4
 #define ERROR 5
 #define OACK 6
 
-// TFTP protocol error codes
+// Erreurs spécifiques au protocol TFTP
 #define UNDEF 0
 #define FILNF 1
 #define ILLEG 4
 #define UNKNW 5
 
-// Error codes
+// Erreurs de routines
 #define NERRORS 8
 #define EARGU 1
 #define ERRCO 2
@@ -33,7 +33,7 @@
 #define EUNKN 7
 #define ENOPA 8
 
-// Other constants
+// Configuration du serveur
 #define TIMEOUT 5
 #define MAX_TRIES 3
 #define MIN_BLKSIZE 8
@@ -41,13 +41,15 @@
 #define MIN_WINDOWSIZE 1
 #define MAX_WINDOWSIZE 65535
 
-int tftp_get_opt(char *packet, size_t *nbytes, size_t *nblocks);
+// Routines qui fabriquent des paquets
 int tftp_make_ack(char *buffer, size_t *length, uint16_t block);
 int tftp_make_OACK(char *buffer, size_t *length, size_t nbytes, size_t nblocks);
 int tftp_make_rrq(char *buffer, size_t *length, const char *file);
 int tftp_make_rrq_opt(char *buffer, size_t *length, const char *fichier, size_t nbytes, size_t nblocs);
 int tftp_make_data(char *buffer, size_t *length, uint16_t block, const char *data, size_t n);
 int tftp_make_error(char *buffer, size_t *length, uint16_t errcode, const char *message);
+
+// Routines de communication
 int tftp_wait_DATA_with_timeout(SocketUDP *socket, AdresseInternet *from, char *res, size_t *reslen);
 int tftp_send_error(SocketUDP *socket, const AdresseInternet *dst, uint16_t code, const char *msg);
 int tftp_send_OACK(SocketUDP *socket, const AdresseInternet *dst, char *packet, size_t packlen);
@@ -57,6 +59,16 @@ int tftp_send_RRQ_wait_DATA(SocketUDP *socket, const AdresseInternet *dst, const
 int tftp_send_DATA_wait_ACK(SocketUDP *socket, const AdresseInternet *dst, const char *packet, size_t packlen);
 int tftp_send_ACK_wait_DATA(SocketUDP *socket, const AdresseInternet *dst, const char *packet, size_t packlen, char *res, size_t *reslen);
 int tftp_send_last_ACK(SocketUDP *socket, const AdresseInternet *dst, const char *packet, size_t packlen);
+
+// Routines qui affichent des paquets
+int tftp_print(char *packet);
+int tftp_print_RRQ(char *packet);
+int tftp_print_ACK(char *packet);
+int tftp_print_OACK(char *packet);
+int tftp_print_DATA(char *packet);
+
+// Routines utilitaires
+int tftp_get_opt(char *packet, size_t *nbytes, size_t *nblocks);
 char *tftp_strerror(ssize_t errcode);
 
 #endif
