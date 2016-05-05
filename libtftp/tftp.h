b13478798c3e930de2321066e6187aa3fd71b50f
Mathieu Brochard <mathieu.brochard76@free.fr>
2016-05-06 00:40:07 +0200

    Ajout commentaires des routines

--- libtftp/tftp.h
+++ libtftp/tftp.h
@@ -43,19 +43,47 @@ struct tftp_error {
     char *errmsg;
 };
 
-/* Routines qui fabriquent des structures TFTP */
+/* Fabrique une structure qui représente un paquet RRQ.
+ * Les options sont optionnelles.
+ * Les options sont données dans un tableau de chaînes de caractères.
+ * Le nom des options sont suivis de leur valeur.
+ * nopt est le nombre d'options (sans compter les valeurs des options).
+ * Libérez la structure après utilisation avec tftp_free_rrq.
+ */
 int tftp_make_rrq(struct tftp_rrq *buf, const char *filename, const char *mode,
     const char **opt, size_t nopt);
+
+/* Fabrique une structure qui représente un paquet DATA.
+ * Libérez la structure après utilisation avec tftp_free_data.
+ */
 int tftp_make_data(struct tftp_data *buf, uint16_t block, uint8_t *data,
     size_t len);
+
+/* Fabrique une structure qui représente un paquet ACK.
+ */
 int tftp_make_ack(struct tftp_ack *buf, uint16_t block);
+
+/* Fabrique une structure qui représente un paquet OACK.
+ * Les options sont données dans un tableau de chaînes de caractères.
+ * Le nom des options sont suivis de leur valeur.
+ * nopt est le nombre d'options (sans compter les valeurs des options).
+ * Libérez la structure après utilisation avec tftp_free_oack.
+ */
 int tftp_make_oack(struct tftp_oack *buf, const char **opt, size_t nopt);
+
+/* Fabrique une structure qui représente un paquet ERROR.
+ * Libérez la structure après utilisation avec tftp_free_error.
+ */
 int tftp_make_error(struct tftp_error *buf, uint16_t errcode,
     const char *errmsg);
 
-/* Routines qui libèrent des structures TFTP */
+/* Libère une structure RRQ. */
 int tftp_free_rrq(struct tftp_rrq *buf);
+
+/* Libère une structure DATA. */
 int tftp_free_data(struct tftp_data *buf);
+
+/* Libère une structure ERROR. */
 int tftp_free_error(struct tftp_error *buf);
 
 #endif
